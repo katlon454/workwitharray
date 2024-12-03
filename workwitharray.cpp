bd1fb91c2a70cbc2ad8aaf4e3e687c7203e5922f
user <user@vlk.top>
2024-12-03 21:04:24 +0300

    fill and show array

--- workwitharray.cpp
+++ workwitharray.cpp
@@ -2,5 +2,14 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    const int size = 8;
+    int mas[size];
+    for (int i = 0; i < size; i++) {
+        mas[i] =size - i;
+       
+    }
+    for (int i = 0; i < 8; i++) {
+        std::cout<<mas[i] << ",";
+    }
+    std::cout << (char)8;
 }
