f9b8caa1734e00433ecabdd05687bd538682da09
user <user@vlk.top>
2024-12-10 21:21:35 +0300

    half finished

--- workwitharray.cpp
+++ workwitharray.cpp
@@ -4,30 +4,53 @@ void show_array(int s,int* m) {
     for (int i = 0; i < s; i++) {
         std::cout << m[i] << ",";
     }
-    std::cout << (char)8;
+    std::cout << (char)8<<'\n';
 }
 
 void fill_array(int* m,int size) {
     for (int i = 0; i < size; i++) {
-        if (i % 2 == 0) {
-            if (rand() % 2 == 0) {
 
-            }
-        }
-        else {
-            if()
-        }
-            m[i] = rand() % 1001;
+        m[i] = rand() % 100;
     }
 }
 
+int sort(int* m,int size) {
+    int i = 0;
+    int reg = 0;
+    bool flag=true;
+    while (flag==true) {
+        flag = false;
+        for (i = 0; i < size-1;i++) {
+            if (m[i] > m[i + 1]) {
+                reg = m[i];
+                m[i] = m[i + 1];
+                m[i + 1] = reg;
+                flag = true;
+            }
+        }
+    }
+    return *m;
+}
 
+void sort2(int* m, int size) {
+    int i, j;
+    int min;
+    for (i = 0; i < size - 1; i++); {
+        min = 101;
+        for (j = i; j < size - 1; j++) {
+            if (min > m[j]) {
+                min = m[j];
+            }
+        }
+    }
+}
 
 int main()
 {
-    const int size = 500;
+    const int size = 10;
     int mas[size];
     fill_array(mas, size);
     show_array(size,mas);
-    
+    sort(mas, size);
+    show_array(size, mas);
 }
